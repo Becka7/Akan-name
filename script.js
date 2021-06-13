214f30de5bd085f64b73f19f2b2eb201a2b0432a
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-06-13 14:46:04 +0300

    validate all the input fields

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 function myFunction() {
-    // alert("Here")
     var birthday=parseInt(document.getElementById("birthdate").value);
+    if(birthday.toString().length >2 || birthday.toString()>31){
+        alert("Please enter a valid day")
+    }
     var birthmonth= parseInt(document.getElementById("birthmonth").value);
+    if(birthmonth.toString()>12 ){
+        alert("Please enter a valid month")
+    }
     var birthyear= parseInt(document.getElementById("birthyear").value);
+    if(birthyear.toString().length !== 4){
+        alert("Please enter a valid year")
+    }
     var d = new Date( birthyear,birthmonth,birthday);
     var n = d.getDay();
+
+    
+
+    
 
     var femaleAkans=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     var maleAkans=["Kwasi","Kwado" ,"Kwabena ","Kwaku","Yaw" ,"Kofi","Kwame"];
@@ -19,11 +31,20 @@ function myFunction() {
         // alert("HEEYYY")
         document.getElementById("akonName").innerHTML = femaleAkans[n];
 
-    }else if(document.getElementById("male").notchecked && document.getElementById("female").notchecked){
-        alert("You can only check one")
     }
+    else{
+       alert("Please select your gender");
+    }
+    // else if(document.getElementById("male").notchecked && document.getElementById("female").notchecked){
+    //     alert("You can only check one")
+    // }
 }
-
+// function validateForm(){
+//     var b= (document.forms[birthForm].[birthdate].value);
+//     if(b==""){
+//     alert("please enter valid year")
+// }
+// }
 
 
     // document.getElementById("akonName").innerHTML = n;
